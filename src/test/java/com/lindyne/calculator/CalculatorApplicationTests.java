a472ebb6ba67ccbc8c3904f58b2e079ecc364368
PB <lindynetech@gmail.com>
2021-03-22 11:43:50 -0400

    Add Spring Boot skeleton

---
+++ src/test/java/com/lindyne/calculator/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindyne.calculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
